8b5425e6fc05f2de318b8cdd09f7c0dbfad36287
Josue Martinez Fernandez <jmartnz.designer@gmail.com>
2018-09-23 23:42:26 +0200

    Added player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] private float moveSpeed = 15.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    private void Move()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.position = new Vector2(transform.position.x + deltaX, transform.position.y + deltaY);
+    }
+}
