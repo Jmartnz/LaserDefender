1f948306d7846869bd2ecad052249fd1d714143b
Josue Martinez Fernandez <jmartnz.designer@gmail.com>
2018-09-24 00:11:32 +0200

    Set up move boundaries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,25 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
 
     [SerializeField] private float moveSpeed = 15.0f;
+    [SerializeField] private float padding = 1.0f;
+
+    private float xMin;
+    private float xMax;
+    private float yMin;
+    private float yMax;
 
 	// Use this for initialization
 	void Start () {
-		
+        SetUpMoveBoundaries();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
         Move();
 	}
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
 
     private void Move()
     {
         float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        transform.position = new Vector2(transform.position.x + deltaX, transform.position.y + deltaY);
+        transform.position = new Vector2(
+            Mathf.Clamp(transform.position.x + deltaX, xMin, xMax),
+            Mathf.Clamp(transform.position.y + deltaY, yMin, yMax)
+        );
     }
 }
