c2433e5b1fed30083fbb77e9ad19d626157d4397
Josue Martinez Fernandez <jmartnz.designer@gmail.com>
2018-10-09 02:09:36 +0200

    Working on start menu

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,17 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] private AudioClip levelClip;
+
+    // Use this for initialization
+    private void Start() {
+        
+    }
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void ExitGame()
+    {
+        // Application.Quit();
+    }
 
 }
